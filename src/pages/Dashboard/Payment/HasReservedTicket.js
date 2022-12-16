4811e1f665b582429f41e0738a090aa8ca703739
danielpqb <danielpqb@gmail.com>
2022-12-16 19:57:28 -0300

    feat: render new screen if user has reserved ticket

---
+++ src/pages/Dashboard/Payment/HasReservedTicket.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export default function HasReservedTicket({ ticket }) {
+  console.log(ticket);
+
+  return <Container>Ana Paula</Container>;
+}
+
+const Container = styled.div``;
