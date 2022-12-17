97dcdb58e0b3c729a200e6caf506e16518d26d14
danielpqb <danielpqb@gmail.com>
2022-12-17 14:41:58 -0300

    feat: activities screen message

--- src/pages/Dashboard/Activities/index.js
+++ src/pages/Dashboard/Activities/index.js
@@ -1,3 +1,47 @@
+import styled from 'styled-components';
+import useTicket from '../../../hooks/api/useTicket';
+
 export default function Activities() {
+  const { ticket } = useTicket();
+
+  if (ticket?.status !== 'PAID') {
+    return (
+      <Container>
+        <Title>Atividades</Title>
+        <Message>Você precisa de um ticket pago para acessar as atividades.</Message>
+      </Container>
+    );
+  }
+
   return 'Atividades: Em breve!';
 }
+
+const Container = styled.div`
+  & {
+    display: flex;
+    flex-direction: column;
+
+    height: 100%;
+  }
+`;
+
+const Title = styled.div`
+  & {
+    font-size: 34px;
+    line-height: 40px;
+
+    margin-bottom: 30px;
+  }
+`;
+
+const Message = styled.div`
+  & {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    
+    height: 100%;
+    
+    margin-bottom: 30px;
+  }
+`;
