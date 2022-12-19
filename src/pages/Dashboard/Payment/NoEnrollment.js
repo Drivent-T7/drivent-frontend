c24e8305eee2483e8bfa21383237e00a4d9cb607
anapairo <ana.pairo2@gmail.com>
2022-12-19 17:16:11 -0300

    feat: noEnrollment component of payment screen implemented

---
+++ src/pages/Dashboard/Payment/NoEnrollment.js
@@ -0,0 +1,33 @@
+import { Title } from './styles/CommomStyle';
+import styled from 'styled-components';
+
+export default function NoEnrollment() {
+  return (
+    <>
+      <Title>Ingresso e Pagamento</Title>
+      <AlertWrapper>
+        <p>Você precisa completar sua inscrição antes de prosseguir pra escolha de ingresso</p>
+      </AlertWrapper>
+    </>
+  );
+}
+
+const AlertWrapper = styled.div`
+  & {
+    width: 100%;
+    height: 70%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  p {
+    width: 50%;
+
+    font-family: 'Roboto', sans-serif;
+    font-size: 20px;
+    line-height: 23px;
+    color: #8e8e8e;
+    text-align: center;
+  }
+`;
--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,10 +1,17 @@
 import Splash from '../../../components/Splash';
 import useTicket from '../../../hooks/api/useTicket';
+import useEnrollment from '../../../hooks/api/useEnrollment';
 import NoTicket from './NoTicket';
 import HasReservedTicket from './HasReservedTicket';
+import NoEnrollment from './NoEnrollment';
 
 export default function Payment() {
   const { ticket, ticketLoading, getTicket } = useTicket();
+  const { enrollment } = useEnrollment();
+
+  if (!enrollment) {
+    return <NoEnrollment />;
+  }
 
   if (ticketLoading) {
     return <Splash loading />;
